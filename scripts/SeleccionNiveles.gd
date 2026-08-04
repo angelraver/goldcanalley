0cd6d67e9755f2f80b419c28e5cec8a7eaefa882
Angel Florentin <angelflorentin@Angels-MacBook-Pro-2.local>
2026-08-04 17:48:51 -0300

    title screen

--- scripts/SeleccionNiveles.gd
+++ scripts/SeleccionNiveles.gd
@@ -107,3 +107,6 @@ func _on_nivel_seleccionado(numero_nivel: int) -> void:
 	print("Cargando Nivel: ", numero_nivel)
 	SaveManager.nivel_actual_seleccionado = numero_nivel
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_boton_home_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Title.tscn")
