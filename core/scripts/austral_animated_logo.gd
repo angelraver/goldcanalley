81b1fc25eabbbc95d445d6c559c18167a967f247
Angel Florentin <angeldavidflorentin@gmail.com>
2026-09-04 11:45:23 -0300

    fixing bugs

--- core/scripts/austral_animated_logo.gd
+++ core/scripts/austral_animated_logo.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var label_mat = $TextureRect/Label.material as ShaderMaterial
 @onready var crt_mat = $"../CRT_EFFECT".material as ShaderMaterial
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var t = Time.get_ticks_msec() / 5000.0
 	
 	if label_mat:
